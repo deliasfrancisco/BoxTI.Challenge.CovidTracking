a64406f8b75e0e67cc53bce879ec4b9600dd4a5e
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:44:53 -0300

    Criação de entidades do contexto

---
+++ BoxTI.Challenge.CovidTracking.Models/Entities/Covid.cs
@@ -0,0 +1,33 @@
+﻿using BoxTI.Challenge.CovidTracking.Models.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BoxTI.Challenge.CovidTracking.Models.Entities
+{
+	public class Covid : BaseEntity
+	{
+		public int Infected { get; private set; }
+		public int Dead { get; private set; }
+		public int Recovered { get; private set; }
+		public int Vaccinated { get; private set; }
+		public int CountryId { get; private set; }
+		public Country Country { get; private set; }
+
+		public Covid(int infected, int dead, int recovered, int vaccinated, int countryId)
+		{
+			DomainException.ToThrow(infected >= 0, "Infected amount invalid");
+			DomainException.ToThrow(dead >= 0, "Dead amount invalid");
+			DomainException.ToThrow(recovered >= 0, "Recovered amount invalid");
+			DomainException.ToThrow(vaccinated >= 0, "Vaccinated amount invalid");
+			DomainException.ToThrow(CountryId > 0, "GuiaId invalid");
+
+			Infected = infected;
+			Dead = dead;
+			Recovered = recovered;
+			Vaccinated = vaccinated;
+			CountryId = countryId;
+		}
+	}
+}
