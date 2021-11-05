b7a7b5041893ae39d0addbab36162347c8fe60e3
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:53:12 -0300

    Criação da classe de contexto da aplicação

---
+++ BoxTI.Challenge.CovidTracking.Data/Context/BoxTIContext.cs
@@ -0,0 +1,28 @@
+﻿using BoxTI.Challenge.CovidTracking.Models.Entities;
+using BoxTI.Challenge.CovidTracking.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxTI.Challenge.CovidTracking.Data.Context
+{
+	public class BoxTIContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+	{
+		public BoxTIContext(DbContextOptions<BoxTIContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Country> Countries { get; set; }
+		public DbSet<Covid> Covid { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Covid>().HasKey(c => new { c.Id, c.CountryId });
+		}
+	}
+}
