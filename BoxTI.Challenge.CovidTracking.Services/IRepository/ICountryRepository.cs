18a93eda142ce4ae934a6902d76a8dcddfcbb036
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:47:19 -0300

    Criação de interfaces do repositorio

---
+++ BoxTI.Challenge.CovidTracking.Services/IRepository/ICountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BoxTI.Challenge.CovidTracking.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxTI.Challenge.CovidTracking.Services.IRepository
+{
+	public interface ICountryRepository : IRepositoryBase
+	{
+		Task<List<Country>> GetAll();
+		Task<Country> GetById(int id);
+		Task<Country> GetByCountryName(string name);
+	}
+}
