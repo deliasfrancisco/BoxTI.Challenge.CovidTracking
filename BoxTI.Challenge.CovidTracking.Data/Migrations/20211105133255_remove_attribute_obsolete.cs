6275e5dffded5901e8a5907b3a537a4886804b19
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 14:19:24 -0300

    Migrations remove attribute

---
+++ BoxTI.Challenge.CovidTracking.Data/Migrations/20211105133255_remove_attribute_obsolete.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoxTI.Challenge.CovidTracking.Data.Migrations
+{
+    public partial class remove_attribute_obsolete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Vaccinated",
+                table: "Covid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Vaccinated",
+                table: "Covid",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
