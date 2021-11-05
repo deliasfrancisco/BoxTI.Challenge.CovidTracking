9a5ff75e21fd887961f1cfdbdf1530a0ebe1d7f7
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:46:36 -0300

    Criação classes para a lib para o core identity

---
+++ BoxTI.Challenge.CovidTracking.Models/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BoxTI.Challenge.CovidTracking.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BoxTI.Challenge.CovidTracking.Models.Identity
+{
+	public class User : IdentityUser<int>
+	{
+		[Column(TypeName = "varchar(150)")]
+		public string FullName { get; set; }
+		public List<UserRole> UserRoles { get; set; }
+
+	}
+}
