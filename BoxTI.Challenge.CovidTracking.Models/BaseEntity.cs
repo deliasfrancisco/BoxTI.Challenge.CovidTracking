9fc940a63eeb90ddf7b0c4430d8e002fcc84c659
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:49:57 -0300

    Entidade base

---
+++ BoxTI.Challenge.CovidTracking.Models/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BoxTI.Challenge.CovidTracking.Models
+{
+	public class BaseEntity
+	{
+		[Key]
+		public int Id { get; set; }
+
+		//protected BaseEntity()
+		//{
+		//	Id = new Guid().ToString();
+		//}
+	}
+}
