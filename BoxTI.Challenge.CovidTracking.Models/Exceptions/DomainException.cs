9297bdc98259208face99e1441063b4e9956a509
Diego Francisco <deliasfrancisco@gmail.com>
2021-11-05 09:52:32 -0300

    Classe para tratamento de excessões

---
+++ BoxTI.Challenge.CovidTracking.Models/Exceptions/DomainException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxTI.Challenge.CovidTracking.Models.Exceptions
+{
+	public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message) { }
+
+        public static void ToThrow(bool invalid, string message)
+        {
+            if (invalid)
+                throw new DomainException(message);
+        }
+    }
+}
